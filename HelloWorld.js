fc2991588cb5ab3c842782441983442418b757bc
Arunkumar Shanmugasundaram <aruncps@users.noreply.github.com>
2020-02-10 19:49:19 +0530

    Create HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let
+    name = 'arun'
+  , age=25
+  , message = 'First JS alert message';
+  //, infi = Infinity
+
+document.write(name + " - " + age );
+
+document.write (
+       "<p> First Value Entered: " + num1 + \n
+       "Second Value Entered: " + num2 + \n
+       "Sum: " + addition + \n
+       "Difference: " + subtraction + \n
+       "Product: " + multiplication + \n
+       "Quotient: " + division + "</p>"
+   );
+   
+//document.write(infi);
