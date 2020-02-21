5bae7c2843749f11faba784dc5bf408d201f456e
Shanmugasundaram, Arunkumar <shanmuga@advisory.com>
2020-02-21 19:23:00 +0530

    Chapter#01-Task

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,20 +1,5 @@
 "use strict";
 
-let
-    name = 'arun'
-  , age=25
-  , message = 'First JS alert message';
-  //, infi = Infinity
 
-document.write(name + " - " + age );
-
-document.write (
-       "<p> First Value Entered: " + num1 + \n
-       "Second Value Entered: " + num2 + \n
-       "Sum: " + addition + \n
-       "Difference: " + subtraction + \n
-       "Product: " + multiplication + \n
-       "Quotient: " + division + "</p>"
-   );
-   
-//document.write(infi);
+alert('Im javascript')
+document.write('Im javascript');
