572fd760db4143b1d9a37096e02071368647188e
Shanmugasundaram, Arunkumar <shanmuga@advisory.com>
2021-01-11 20:05:33 +0530

    more learning

---
+++ script.js
@@ -0,0 +1,14 @@
+var firstName='Arun';
+console.log(firstName);
+
+var lastName='Shan';
+var age=39;
+
+var fullAge=true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
